452a5e125f6463d4056a0588eb713db2dc38ebd8
Felipe De Mello Flores <felipe.melloflores@gmail.com>
2018-10-16 14:18:18 -0300

    teste2

--- teste.ml
+++ teste.ml
@@ -8,37 +8,40 @@ let get_time_now () =
   Unix.time ()
 ;;
 
-let draw_world ~player:(x,y) ~enemy:(x2,y2) =
+let draw_world ~player:(x,y) ~enemy:enemy_list =
   clear_graph ();
   fill_rect x y 100 100;
-  fill_rect x2 y2 100 100;
+  List.iter (fun (x2,y2) -> fill_rect x2 y2 50 50) enemy_list;
 ;;
 
+let update_enemys ~enemy:enemy_list old_time new_time =
+  if (new_time -. old_time) > 0.1 then
+    List.map (fun (x,y) -> ((x+20) , y) ) enemy_list
+  else
+    enemy_list
+;;
 
-
-let rec handler ~player:(x,y) ~enemy:(x2,y2) old_time =
+let rec handler ~player:(x,y) ~enemy:enemy_list old_time =
   
-  draw_world ~player:(x,y) ~enemy:(x2,y2);
+  draw_world ~player:(x,y) ~enemy:enemy_list;
 
   let new_time = get_time_now () in
-    
-    if (new_time -. old_time) > 0.05 then 
-      handler ~player:(x,y) ~enemy:((x2+10),y2) new_time;
- 
+    let enemy' = update_enemys ~enemy:enemy_list old_time new_time in
+    let time' = if (new_time -. old_time) > 0.1 then new_time else old_time in
 
   let event = Graphics.wait_next_event [ Graphics.Poll ] in
     if event.Graphics.keypressed then
       match (read_key ()) with
-      |'a' -> handler ~player:((x-20),y) ~enemy:(x2,y2) old_time
-      |'d' -> handler ~player:((x+20),y) ~enemy:(x2,y2) old_time
-      |'w' -> handler ~player:(x,(y+20)) ~enemy:(x2,y2) old_time
-      |'s' -> handler ~player:(x,(y-20)) ~enemy:(x2,y2) old_time
-      |'q' -> handler ~player:(300,100) ~enemy:(x2,y2) old_time
-      | _ -> handler ~player:(x,y) ~enemy:(x2,y2) old_time
+      |'a' -> handler ~player:((x-20),y) ~enemy:enemy' time'
+      |'d' -> handler ~player:((x+20),y) ~enemy:enemy' time'
+      |'q' -> handler ~player:(300,100) ~enemy:enemy' time'
+      | _ -> handler ~player:(x,y) ~enemy:enemy' time'
     else
-      handler ~player:(x,y) ~enemy:(x2,y2) old_time
+      handler ~player:(x,y) ~enemy:enemy' time'
+  
 ;;
 
 loop_at_exit [Key_pressed ; Button_down]
    (fun event ->
-      handler ~player:(300,100) ~enemy:(100,300) 0.0);;
+      handler ~player:(300,100) ~enemy:[(50,500);(120,500)] 0.0)
+;;
