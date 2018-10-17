b2e446ea60e116d559e168458d9ac865f394bfc6
Felipe Flores <felipe.melloflores@gmail.com>
2018-10-17 15:31:07 -0300

    rmv update configs

--- update.ml
+++ update.ml
@@ -1,19 +1,7 @@
 open Config;;
 open Utilities;;
 
-(* bullet *)
-let _bullet_speed = 0.05;;
 
-let _bullet_step_distance = 10;;
-let _bullet_rate = 1.0;;
-
-(* enemy *)
-
-let _enemy_speed = 0.2;;
-
-let _enemy_step_distance = 20;;
-let _enemy_downstep_distance = 30;;
-let _enemy_area_border = (20,870) ;;
 
 
 
