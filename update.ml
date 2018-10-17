269d16bcfbb455cc0afe134f0bdea744ea0f56e8
GABRIELMUTTI <gabrielmuttiteixeira@gmail.com>
2018-10-17 20:09:31 -0300

    Arrumado um erro nos inimigos.

--- update.ml
+++ update.ml
@@ -35,13 +35,26 @@ let check_bullet_enemy_collision state =
 
 
 
-let clear_bullets_out_of_screen state =
+let clear_bullet_out_of_screen state =
   let (bx, by) = state.bullet in
   if by > snd _screen_size then
     { state with bullet_on = false; bullet_delay = 0.0 }
   else
     state
+;;
 
+let clear_enemy_bullets_out_of_screen state =
+  let rec aux_loop bullets =
+    match bullets with
+    | [] -> []
+    | (ex, ey) :: tl ->
+       if ey < 0 then
+         aux_loop tl
+       else
+         List.cons (ex, ey) (aux_loop tl) in
+  let bullets' = aux_loop state.enemy_bullets in
+  { state with enemy_bullets = bullets' }
+;;
 
 (*** UPDATE FUNCTIONS ***)
 let update_enemies_right state dt =
@@ -108,8 +121,12 @@ let update_enemy_bullets state dt =
 ;;
 
 let select_random_enemy enemies =
-  let random_index = Random.int ((List.length enemies) - 1) in
-  List.nth enemies random_index
+  if List.length enemies > 1 then
+    (let random_index = Random.int ((List.length enemies) - 1) in
+     List.nth enemies random_index)
+  else
+    List.nth enemies 0
+
 ;;
 
 let fire_enemy_bullet state dt =
@@ -121,23 +138,24 @@ let fire_enemy_bullet state dt =
     { state with enemy_fire_delay = state.enemy_fire_delay +. dt }
 ;;    
 
-let check_game_over state =
-  if ((snd (enemy_far_down state.enemies)) <= _game_over_line )then 
-    {state with game_over = true}
-    else state
-;;
 
 let update_state state dt =
-  let state' = if state.enemy_direction then update_enemies_right state dt 
-               else update_enemies_left state dt in
-  let state2' = if state.bullet_on then update_bullet state' dt else state' in
+  let state1' = if List.length state.enemies > 0 then
+                        (let enemy_state1' = if state.enemy_direction then update_enemies_right state dt 
+                                            else update_enemies_left state dt in
+                        let enemy_state2' = fire_enemy_bullet enemy_state1' dt in
+                        enemy_state2')
+                else
+                  state in
+                        
+                        
+  let state2' = if state.bullet_on then update_bullet state1' dt else state1' in
   let state3' = check_player_boundaries state2' in
   let state4' = check_bullet_enemy_collision state3' in
-  let state5' = clear_bullets_out_of_screen state4' in
-  let state6' = fire_enemy_bullet state5' dt in
-  let state7'= update_enemy_bullets state6' dt in
-  let state8' = check_game_over state7' in
-  state8'
+  let state5' = clear_bullet_out_of_screen state4' in
+  let state6' = clear_enemy_bullets_out_of_screen state5' in
+  let state7' = update_enemy_bullets state6' dt in
+  state7'
 ;;
 
 let fire_bullet state =
